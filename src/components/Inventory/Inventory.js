d40b46e964a9a43331bd226ac68355d39e211cec
hafijur-rahman1 <61210448+hafijur-rahman1@users.noreply.github.com>
2021-10-24 17:07:18 +0600

    update inventory

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h3>this is Inventory</h3>
+            <h3>this is Inventory .and this is comming soon</h3>
         </div>
     );
 };
